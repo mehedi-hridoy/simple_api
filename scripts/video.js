8567129cff1b3f2e502935e982ac225a693b2149
mehedi-hridoy <mehedi.hridoy101@gmail.com>
2025-08-28 17:56:47 +0600

    API With JS

---
+++ scripts/video.js
@@ -0,0 +1,172 @@
+console.log("video script added");
+//fetch , load and show categories on html
+function getTimeString(time) {
+  const hour = parseInt(time / 3600);
+  let remainingSecond = time % 3600;
+  const minute = parseInt(remainingSecond / 60);
+  remainingSecond = remainingSecond % 60;
+  return `${hour} hour ${minute} minute ${remainingSecond} seconds`;
+}
+
+const removeActiveClass = () => {
+  const buttons = document.getElementsByClassName("category-btn");
+
+  for (let btn of buttons) {
+    btn.classList.remove("active");
+  }
+};
+// create loadCategories
+const loadCategories = () => {
+  //fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
+
+const loadVideos = (searchText = "") => {
+  //fetch the data
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.log(error));
+};
+
+const loadCategoryVideos = (id) => {
+  // alert(id);
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //remove active class
+      removeActiveClass();
+
+      // active the class of id
+      const activeBtn = document.getElementById(`btn-${id}`);
+      activeBtn.classList.add("active");
+      displayVideos(data.category);
+    })
+    .catch((error) => console.log(error));
+};
+
+const loadDetails = async(videoId) => {
+    console.log(videoId);
+    const uri = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    const res = await fetch(uri);
+
+    const data = await  res.json();
+    console.log(data);
+    displayDetails(data.video)
+}
+const displayDetails=(video)=> {
+
+    const detailContainer = document.getElementById("modal-content");
+    
+
+    detailContainer.innerHTML = `
+     <img src=${video.thumbnail} />
+     <p>${video.description}</p>
+
+    `
+
+    // way - 1 
+    document.getElementById("showModalData").click();
+
+}
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.classList.remove("grid");
+    videoContainer.innerHTML = `
+    <div class="min-h-[300px] w-full flex flex-col gap-5 justify-center items-center">
+     
+     <img class="text-center text-xl font-bold" src="assets/Icon.png"  />
+     <h2>No Content Here in this Category </h2>
+    </div>
+    `;
+    return;
+  } else {
+    videoContainer.classList.add("grid");
+  }
+  videos.forEach((video) => {
+    // console.log("video");
+
+    const card = document.createElement("div");
+    card.classList = "card bg-base-100 ";
+    card.innerHTML = `
+      <figure class="h-[200px] relative">
+    <img
+      src=${video.thumbnail}
+      class="h-full w-full object-cover"
+      alt="Shoes" />
+      ${
+        video.others.posted_date?.length == 0
+          ? " "
+          : `
+          <span class="absolute right-2 bottom-2 bg-black rounded p-1 text-white">${getTimeString(
+            video.others.posted_date
+          )} </span> 
+
+        `
+      }
+    
+
+  </figure>
+  <div class="px-0 py-2 flex gap-2">
+      <div>
+        <img class="w-10 h-10 rounded-full object-cover" src=${
+          video.authors[0].profile_picture
+        } />
+
+      </div>
+
+      <div>
+         <h2 class="font-bold">${video.title}</h2>
+         <div class="flex items-center gap-2">
+         <p class="text-gray-400">${video.authors[0].profile_name} </p>
+         ${
+           video.authors[0].verified == true
+             ? `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"
+`
+             : ""
+         }
+         </div>
+         <p> <button onclick="loadDetails('${video.video_id}')" class="btn btn-sm btn-error"> details </button> </p>
+        
+      </div>
+    
+  </div>
+
+        `;
+    videoContainer.append(card);
+  });
+};
+
+//Create Display categoreis
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById("categories");
+
+  categories.forEach((item) => {
+    console.log(item);
+    //create a button
+    const buttonContainer = document.createElement("div");
+    buttonContainer.innerHTML = `
+    <button id="btn-${item.category_id}" onclick="loadCategoryVideos(${item.category_id})" class="btn category-btn">
+    ${item.category}
+
+    <button>
+    `;
+
+    // add button to category container
+    categoryContainer.append(buttonContainer);
+  });
+};
+
+
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+   loadVideos(e.target.value)
+});
+loadCategories();
+loadVideos();
---
+++ summary.js
@@ -0,0 +1,17 @@
+/*
+1. load and display data initially
+2. on click load and display data
+3. complex object acessing 
+4. toggle active class functionality
+5. load and display single data
+6. display data in a modal 
+7. load and display single category data
+8. conditional rendering if array don't have any data
+9. conditional rendering if a value empty 
+10. conditional rendering if a value length is zero
+
+
+
+
+
+*/
